0f7f116bb227ef18ed82538802c70919aaab07ba
99pmore <morenomartin.pablo@gmail.com>
2023-12-15 21:07:27 +0100

    added card component

--- Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
+++ Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { CardComponent } from 'src/app/components/card/card.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, CardComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
