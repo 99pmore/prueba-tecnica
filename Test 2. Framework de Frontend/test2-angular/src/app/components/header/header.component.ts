8c6c404fa2235e3caba59dd8b866a48e29265108
99pmore <morenomartin.pablo@gmail.com>
2023-12-15 20:36:54 +0100

    fontawesome icons changed to angular ones

--- Test 2. Framework de Frontend/test2-angular/src/app/components/header/header.component.ts
+++ Test 2. Framework de Frontend/test2-angular/src/app/components/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  public faCartShopping = faCartShopping
+  public faMagnifyingGlass = faMagnifyingGlass
+  public faBars = faBars
 }
