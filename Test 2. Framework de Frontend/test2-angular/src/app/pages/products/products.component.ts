689f636b78df89b7ccc7c3df70173bd9704ff15f
99pmore <morenomartin.pablo@gmail.com>
2023-12-16 11:19:24 +0100

    added filters button component

--- Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
+++ Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { CardComponent } from 'src/app/components/card/card.component';
+import { FiltersBtnComponent } from 'src/app/components/filters-btn/filters-btn.component';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/product.interface';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, CardComponent],
+  imports: [CommonModule, HeaderComponent, CardComponent, FiltersBtnComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
