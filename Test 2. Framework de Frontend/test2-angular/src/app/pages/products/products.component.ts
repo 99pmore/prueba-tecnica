ad5f7aea4b91523854551bb624287e0f2136c4c1
99pmore <morenomartin.pablo@gmail.com>
2023-12-16 12:00:32 +0100

    added filters component and its appearance functionality

--- Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
+++ Test 2. Framework de Frontend/test2-angular/src/app/pages/products/products.component.ts
@@ -3,26 +3,32 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { CardComponent } from 'src/app/components/card/card.component';
 import { FiltersBtnComponent } from 'src/app/components/filters-btn/filters-btn.component';
+import { FiltersComponent } from 'src/app/components/filters/filters.component';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/product.interface';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, CardComponent, FiltersBtnComponent],
+  imports: [CommonModule, HeaderComponent, CardComponent, FiltersBtnComponent, FiltersComponent],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
-  public products: Product[] = []
+  public products: Product[] = [];
+  public filtersBtnClicked: boolean = false;
 
   constructor(
     private productsService: ProductsService
   ) {}
 
   ngOnInit(): void {
-    this.productsService.getProducts().subscribe((products) => this.products = products)
+    this.productsService.getProducts().subscribe((products) => this.products = products);
+  }
+
+  public toggleFilters() {
+    this.filtersBtnClicked = !this.filtersBtnClicked;
   }
 
 }
