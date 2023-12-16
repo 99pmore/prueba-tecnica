5e90f8c08dde9962358d72860a45c9d080fd0a0e
99pmore <morenomartin.pablo@gmail.com>
2023-12-16 23:40:34 +0100

    added filter by category functionality

--- Test 3. PHP/test3-laravel/app/Http/Controllers/ProductController.php
+++ Test 3. PHP/test3-laravel/app/Http/Controllers/ProductController.php
@@ -19,7 +19,16 @@ class ProductController extends Controller
             'verify' => false
         ])->get($this->apiBaseUrl);
         
-        $products = $response->json();
+        $responseData = $response->json();
+        $products = $responseData['products'];
+
+        if ($request->has('category')) {
+            $category = $request->input('category');
+            $products = array_filter($products, function ($product) use ($category) {
+                return isset($product['category']) && $product['category'] === $category;
+            });
+        }
+
         return $products;
     }
 
