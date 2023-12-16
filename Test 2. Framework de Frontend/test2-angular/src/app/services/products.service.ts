b9dcfe00e49af8ac855f1f63c688ab3ed695ba45
99pmore <morenomartin.pablo@gmail.com>
2023-12-16 11:01:59 +0100

    created service to get products data

--- Test 2. Framework de Frontend/test2-angular/src/app/services/products.service.ts
+++ Test 2. Framework de Frontend/test2-angular/src/app/services/products.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>('../../assets/products/products.json');
+  }
 }
