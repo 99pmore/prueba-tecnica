0711c30677bdef51cc5ec579ab8906efc2921a59
99pmore <morenomartin.pablo@gmail.com>
2023-12-16 11:19:03 +0100

    created filters button component

---
+++ Test 2. Framework de Frontend/test2-angular/src/app/components/filters-btn/filters-btn.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-filters-btn',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './filters-btn.component.html',
+  styleUrls: ['./filters-btn.component.scss']
+})
+export class FiltersBtnComponent {
+
+  public faFilter = faFilter
+
+  public toggleFilters() {
+    
+  }
+}
