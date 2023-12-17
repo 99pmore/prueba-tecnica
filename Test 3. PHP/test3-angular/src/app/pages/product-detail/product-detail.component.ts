189fb6ebaf6e6435075c4e7cb96d36a73a6d569b
99pmore <morenomartin.pablo@gmail.com>
2023-12-17 05:16:08 +0100

    product detail info page done

--- Test 3. PHP/test3-angular/src/app/pages/product-detail/product-detail.component.ts
+++ Test 3. PHP/test3-angular/src/app/pages/product-detail/product-detail.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/models/product.interface';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FontAwesomeModule, RouterLink],
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+
+  public product!: Product;
+
+  public faChevronLeft = faChevronLeft
+
+  private id!: string
+
+  constructor(
+    private productsService: ProductsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.id = this.activatedRoute.snapshot.paramMap.get('id') || ''
+    await this.getProduct(this.id);
+  }
+
+  private async getProduct(id: string) {
+    this.productsService.getProduct(this.id).subscribe((product) => {
+      this.product = product;
+    })
+  }
 
 }
