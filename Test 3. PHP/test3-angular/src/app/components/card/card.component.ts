ef1385309e376a9aab97a0ab0f146682abda9a70
99pmore <morenomartin.pablo@gmail.com>
2023-12-17 05:15:47 +0100

    added router link

--- Test 3. PHP/test3-angular/src/app/components/card/card.component.ts
+++ Test 3. PHP/test3-angular/src/app/components/card/card.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from 'src/app/models/product.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
 
-  @Input() product !: Product
+  @Input() product !: Product;
 }
