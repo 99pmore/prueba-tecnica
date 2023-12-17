93a93c22cf65a041c52633930c3c2b7ec2fe4cc8
99pmore <morenomartin.pablo@gmail.com>
2023-12-17 05:14:51 +0100

    added routes

--- Test 3. PHP/test3-angular/src/app/app.routes.ts
+++ Test 3. PHP/test3-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProductsComponent },
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: '**', pathMatch: 'full', redirectTo: ''}
+];
